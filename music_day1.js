3199e220b21cc04d04f3c8f1fd16ac484174aeb6
akhila1534 <akhilachandrakkantha@gmail.com>
2025-09-09 09:58:41 +1200

    trying to make a cloud

--- music_day1.js
+++ music_day1.js
@@ -1,7 +1,23 @@
+
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-    background(0);
+let Xcol = map (mouseX, 0, width, 0, 255);
+let Ycol = map(mouseY, 0, height, 0, 255);
+background (Xcol, Ycol, 255)
+// for (let x=0; x<5; x++) {
+//     let Space = 30*x
+//     circle (20,10+Space,30)
 
+// }
+ //background 
+ 
+ 
+ //ellipse 
+ fill (255)
+circle (mouseX,mouseY,40)
 
+//cloud
+fill(255)
+circle (5,200,20)
 
 
 }
